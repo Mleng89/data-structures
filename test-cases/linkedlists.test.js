36d9e601fe3a6d202a109e183ca0702d37469e6e
Matthew Leng <mleng89@gmail.com>
2021-05-15 01:11:49 -0400

    Moved to a folder for better accessability

--- test-cases/linkedlists.test.js
+++
@@ -1,6 +0,0 @@
-const { expect } = require('@jest/globals');
-const months = require('../data-structures/linkedlists');
-
-test('Linked list should be an object', () => {
-	expect(typeof months).toBe('object');
-});
