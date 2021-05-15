f2961bbe0bc0e60af9b2acb1127e0fd4976b0d30
Matthew Leng <mleng89@gmail.com>
2021-05-15 01:20:06 -0400

    New test case added

--- algorithms/algorithm-tests/twoSum.test.js
+++ algorithms/algorithm-tests/twoSum.test.js
@@ -5,3 +5,9 @@ test('twoSum should be a function', () => {
 	let func = typeof twoSum === 'function';
 	expect(func).toBe(true);
 });
+
+test('twoSum function', () => {
+	let test = [1, 5, 7, 3, 9, 5];
+	const expected = [1, 5];
+	expect(twoSum(6, test)).toStrictEqual(expected);
+});
