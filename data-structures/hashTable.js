44fb29d8e194e2e29ba1a6b9f7d286a0e4f229ce
Matthew Leng <mleng89@gmail.com>
2021-05-18 22:11:06 -0400

    Added new Map()

--- data-structures/hashTable.js
+++ data-structures/hashTable.js
@@ -12,3 +12,22 @@ array.reduce((accumulator,currentValue)=>{
 },{})
 
 */
+
+/*
+hash.set('kyle',1)
+console.log('this is the hash', hash)
+console.log(hash.get('kyle'))
+
+const hash = new Map()
+let array = [1,1,1,1]
+for(let i = 0 ; i< array.length;i++){
+    if(!hash.has(array[i])){
+        console.log(!hash.get(array[i]))
+        hash.set(array[i], 1) 
+    } else {
+        const value = hash.get(array[i]) + 1
+        hash.set(array[i], value)
+    }
+}
+console.log('what is my hash', hash)
+*/
