987bd2ba91e42beb430735518e5451911c50b436
Matthew Leng <mleng89@gmail.com>
2021-05-20 21:51:09 -0400

    fibbonacci added with memo

---
+++ algorithms/fibonacci.js
@@ -0,0 +1,16 @@
+function fibonacci(num) {
+	if (num <= 2) return 1;
+
+	return fibonacci(num - 1) + fibonacci(num - 2);
+}
+
+//DYNAMIC OR USING MEMOIZATION
+function fibonacciMemo(num, memo) {
+	memo = memo || {};
+
+	if (memo[num]) return memo[num];
+	if (num <= 2) return 1;
+
+	return (memo[num] =
+		fibonacciMemo(num - 1, memo) + fibonacciMemo(num - 2, memo));
+}
