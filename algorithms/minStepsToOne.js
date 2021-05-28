653483f8a8c121047bd492658ef9fcbb45b965f3
Matthew Leng <mleng89@gmail.com>
2021-05-28 16:04:23 -0400

    Minimum steps to 1

---
+++ algorithms/minStepsToOne.js
@@ -0,0 +1,15 @@
+/*
+Write a function that returns the minimum steps
+you can take from n to 1
+
+If n can divide by 2, divide by 2
+If n can divide by 3, divide by 3
+Or else, subtract by 1
+
+5-> 4-> 2-> 1 /// n = 5 output =3
+
+n = 10 output 3
+10-> 5-> 4-> 2-> 1 return: 4
+10-> 9-> 3-> 1 return: 3
+*/
+function minStepsToOne(n) {}
