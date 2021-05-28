4de4ce3868f633c47b02d20a00e32578c362d5ba
Matthew Leng <mleng89@gmail.com>
2021-05-28 16:34:29 -0400

    Memoization added

--- algorithms/minStepsToOne.js
+++ algorithms/minStepsToOne.js
@@ -12,4 +12,54 @@ n = 10 output 3
 10-> 5-> 4-> 2-> 1 return: 4
 10-> 9-> 3-> 1 return: 3
 */
-function minStepsToOne(n) {}
+
+/*
+Recursion
+*/
+function minStepsToOne(n) {
+	if (n === 1) return 0;
+	//subtract 1
+	let steps = minStepsToOne(n - 1);
+	//divide 2
+	if (n % 2 === 0) {
+		let div2 = minStepsToOne(n / 2);
+		steps = Math.min(steps, div2);
+	}
+	//divide 3
+	if (n % 3 === 0) {
+		let div3 = minStepsToOne(n / 3);
+		steps = Math.min(steps, div3);
+	}
+	return steps + 1;
+}
+
+console.log(minStepsToOne(100));
+
+/*
+Memoization
+*/
+let memo = {};
+function minStepsToOneM(n) {
+	if (n in memo) {
+		return memo[n];
+	}
+	if (n === 1) return 0;
+	//subtract 1
+	let steps = minStepsToOneM(n - 1);
+	//divide 3
+	if (n % 3 === 0) {
+		let div3 = minStepsToOneM(n / 3);
+		steps = Math.min(steps, div3);
+	}
+	//divide 2
+	if (n % 2 === 0) {
+		let div2 = minStepsToOneM(n / 2);
+		steps = Math.min(steps, div2);
+	}
+
+	memo[n] = steps + 1;
+	// console.log('what is in my memo', memo);
+	return memo[n];
+}
+
+console.log(minStepsToOneM(10));
