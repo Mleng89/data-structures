ddfc3470cbb4f29b003444fa08593ca9d46c3a85
Matthew Leng <mleng89@gmail.com>
2021-06-24 17:39:19 -0400

    romanToInteger tests added

---
+++ algorithms/algorithm-tests/romanToInt.test.js
@@ -0,0 +1,19 @@
+const romanToInt = require('../romanToInt');
+const { expect } = require('@jest/globals');
+
+test('romanToInt is a function', () => {
+	let func = typeof romanToInt === 'function';
+	expect(func).toBe(true);
+});
+
+test('romanToInt test case 1', () => {
+	expect(romanToInt('III')).toStrictEqual(3);
+});
+
+test('romanToInt test case 2', () => {
+	expect(romanToInt('IV')).toStrictEqual(4);
+});
+
+test('romanToInt test case 3', () => {
+	expect(romanToInt('MCCXXXI')).toStrictEqual(1231);
+});
