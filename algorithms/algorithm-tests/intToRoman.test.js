e928c5a265cf9f0310b62af3e347f056840dc7b3
Matthew Leng <mleng89@gmail.com>
2021-06-24 17:44:24 -0400

    intToRoman tests added

---
+++ algorithms/algorithm-tests/intToRoman.test.js
@@ -0,0 +1,15 @@
+const intToRoman = require('../intToRoman');
+const { expect } = require('@jest/globals');
+
+test('intToRoman should be a function', () => {
+	let func = typeof intToRoman === 'function';
+	expect(func).toBe(true);
+});
+
+test('intToRoman test case 1', () => {
+	expect(intToRoman(1231)).toStrictEqual('MCCXXXI');
+});
+
+test('intToRoman test case 1', () => {
+	expect(intToRoman(4)).toStrictEqual('IV');
+});
