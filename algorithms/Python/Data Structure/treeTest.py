45fedeba285755d0b44bc8875c168d2c8506d14b
Matthew Leng <mleng89@gmail.com>
2022-04-06 20:40:52 -0400

    Adding in tree data structure

---
+++ algorithms/Python/Data Structure/treeTest.py
@@ -0,0 +1,65 @@
+class TreeNode:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+def deserialize(arr):
+    if len(arr) == 0:
+        return None
+    root = TreeNode(arr[0])
+    queue = [root]
+
+    for i in range(1, len(arr), 2):
+        curr = queue.pop(0)
+        if arr[i] is not None:
+            curr.left = TreeNode(arr[i])
+            queue.append(curr.left)
+        if arr[i + 1] is not None:
+            curr.right = TreeNode(arr[i + 1])
+            queue.append(curr.right)
+    return root
+
+
+array = [4, 2, 5, 1, 3, None, 7, None, None, None, None, 6, 8]
+treeNode = deserialize(array)
+print(treeNode)
+
+
+def bfs(node):
+    queue = [node]
+    result = []
+
+    while len(queue) > 0:
+        curr = queue.pop(0)
+
+        result.append(curr.value)
+
+        if curr.left is not None:
+            queue.append(curr.left)
+        if curr.right is not None:
+            queue.append(curr.right)
+    return result
+
+
+def dfsPre(node):
+    ...
+
+
+def dfsPost(node):
+    ...
+
+
+def dfsIn(node):
+    ...
+
+
+print(bfs(treeNode))
+# [4,2,5,1,3,7,6,8]
+print(dfsPre(treeNode))
+# [4,2,1,3,5,7,6,8]
+print(dfsIn(treeNode))
+# [1,2,3,4,5,6,7,8]
+print(dfsPost(treeNode))
+# [1,3,2,6,8,7,5,4]
--- data-structures/trees.js
+++ data-structures/trees.js
@@ -2,35 +2,37 @@
 
 // Node class for a binary tree node
 class TreeNode {
-  constructor(value){
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
+	constructor(value) {
+		this.value = value;
+		this.left = null;
+		this.right = null;
+	}
 }
 
 // Generate tree from array
 function deserialize(arr) {
-  if (arr.length === 0) { return null; }
-  let root = new TreeNode(arr[0]);
-  let queue = [root];
-  for(let i = 1; i < arr.length; i += 2) {
-    let current = queue.shift();
-    if (arr[i] !== null) {
-      current.left = new TreeNode(arr[i]);
-      queue.push(current.left);
-    }
-    if (arr[i + 1] !== null && arr[i + 1] !== undefined) {
-      current.right = new TreeNode(arr[i + 1]);
-      queue.push(current.right);
-    }
-  }
-  return root;
+	if (arr.length === 0) {
+		return null;
+	}
+	let root = new TreeNode(arr[0]);
+	let queue = [root];
+	for (let i = 1; i < arr.length; i += 2) {
+		let current = queue.shift();
+		if (arr[i] !== null) {
+			current.left = new TreeNode(arr[i]);
+			queue.push(current.left);
+		}
+		if (arr[i + 1] !== null && arr[i + 1] !== undefined) {
+			current.right = new TreeNode(arr[i + 1]);
+			queue.push(current.right);
+		}
+	}
+	return root;
 }
 
 const array = [4, 2, 5, 1, 3, null, 7, null, null, null, null, 6, 8];
 const treeNode = deserialize(array);
-// console.log(treeNode);
+console.log(treeNode);
 
 /**
  *
@@ -61,63 +63,68 @@ const treeNode = deserialize(array);
 
 //BREADTH FIRST traversal: [4,2,5,1,3,7,6,8]
 function bfs(node) {
-  let queue = [node]
-  let result = []
-  
-  while(queue.length > 0) {
-    let current = queue.shift()
-    
-    result.push(current.value)
-    
-    if(current.left !== null) { queue.push(current.left) }
-    if(current.right !== null) { queue.push(current.right) }
-    
-  }
-  
-  return result
-}
+	let queue = [node];
+	let result = [];
 
+	while (queue.length > 0) {
+		let current = queue.shift();
+
+		result.push(current.value);
+
+		if (current.left !== null) {
+			queue.push(current.left);
+		}
+		if (current.right !== null) {
+			queue.push(current.right);
+		}
+	}
+
+	return result;
+}
 
 //PRE-ORDER DEPTH first traversal: [4,2,1,3,5,7,6,8]
-let resultPre = []
+let resultPre = [];
 function dfsPre(node) {
-  if(node === null) { return; }
-  
-  resultPre.push(node.value)
-  dfsPre(node.left)
-  dfsPre(node.right)
-  
-  return resultPre
-}
+	if (node === null) {
+		return;
+	}
 
+	resultPre.push(node.value);
+	dfsPre(node.left);
+	dfsPre(node.right);
+
+	return resultPre;
+}
 
 //IN-ORDER DEPTH first traversal: [1,2,3,4,5,6,7,8]
-let resultIn = []
+let resultIn = [];
 function dfsIn(node) {
-  if(node === null) { return; }
+	if (node === null) {
+		return;
+	}
 
-  dfsIn(node.left)
-  resultIn.push(node.value)
-  dfsIn(node.right)
-  
-  return resultIn
+	dfsIn(node.left);
+	resultIn.push(node.value);
+	dfsIn(node.right);
+
+	return resultIn;
 }
-
 
 //POST-ORDER DEPTH first traversal: [1,3,2,6,8,7,5,4]
-let resultPost = []
+let resultPost = [];
 function dfsPost(node) {
-  if(node === null) { return; }
+	if (node === null) {
+		return;
+	}
 
-  dfsPost(node.left)
-  dfsPost(node.right)
-  resultPost.push(node.value)
-  
-  return resultPost
+	dfsPost(node.left);
+	dfsPost(node.right);
+	resultPost.push(node.value);
+
+	return resultPost;
 }
 
-
-console.log(bfs(treeNode)) //[4,2,5,1,3,7,6,8]
-console.log(dfsPre(treeNode)) //[4,2,1,3,5,7,6,8]
-console.log(dfsIn(treeNode)) //[1,2,3,4,5,6,7,8]
-console.log(dfsPost(treeNode)) //[1,3,2,6,8,7,5,4]
+// console.log(bfs(treeNode)); //[4,2,5,1,3,7,6,8]
+// console.log(dfsPre(treeNode)); //[4,2,1,3,5,7,6,8]
+// console.log(dfsIn(treeNode)); //[1,2,3,4,5,6,7,8]
+// console.log(dfsPost(treeNode)); //[1,3,2,6,8,7,5,4]
