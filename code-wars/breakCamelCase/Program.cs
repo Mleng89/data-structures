bdd222ef02fbc90a7112c465d62977ce5a209bef
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-10-24 17:31:37 -0400

    C# Break camelCase

---
+++ code-wars/breakCamelCase/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+*/
+
+static string BreakCamelCase(string str)
+{
+char[] strList = str.ToCharArray();
+string result = "";
+
+    foreach(char letter in strList){
+        if(letter != char.ToUpper(letter)){
+            result += letter.ToString();
+        }
+        else{
+            result += " " + letter.ToString();
+        }
+    }
+return result;
+}
+
+
+Console.WriteLine(BreakCamelCase("camelCasing")); // 'camel Casing'
+Console.WriteLine(BreakCamelCase("camelCaseTesting")); // 'camel Case Testing'
