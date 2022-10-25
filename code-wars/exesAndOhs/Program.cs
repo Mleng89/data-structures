b0d89a652f334c29a5b21b915b1f0919426c2770
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-10-25 17:56:16 -0400

    C# Codewars- Exes and Ohs

---
+++ code-wars/exesAndOhs/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+static bool XO (string input){
+char[] inputList = input.ToLower().ToCharArray();
+int xCount = 0;
+int oCount = 0;
+
+foreach(char letter in inputList){
+    if(letter == 'o'){
+        oCount += 1;
+    } else if(letter == 'x'){
+        xCount +=1;
+    }
+}
+return xCount == oCount;
+}
+
+
+Console.WriteLine(XO("xo")); // true
+Console.WriteLine(XO("xxOo")); // true
+Console.WriteLine(XO("xxxm")); // false
+Console.WriteLine(XO("Oo")); //false
+Console.WriteLine(XO("ooom")); // false
